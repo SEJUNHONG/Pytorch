0a32db75c3812ee0deddd3a4ef4837e3f2b2b544
SEJUNHONG <57587248+SEJUNHONG@users.noreply.github.com>
2021-03-03 16:12:15 +0900

    Add commit

---
+++ ResNeXt_pytorch.py
@@ -0,0 +1,80 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn import init
+
+class ResNeXtBottleneck(nn.Module):
+    def __init__(self, in_channels, out_channels, stride, cardinality, base_width, widen_factor):
+        super(ResNeXtBottleneck, self).__init__()
+        width_ratio=out_channels/(widen_factor*64.)
+        D=cardinality*int(base_width*width_ratio)
+        self.conv_reduce=nn.Conv2d(in_channels, D, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn_reduce=nn.BatchNorm2d(D)
+        self.conv_conv=nn.Conv2d(D, D, kernel_size=3, stride=stride, padding=1, groups=cardinality, bias=False)
+        self.bn=nn.BatchNorm2d(D)
+        self.conv_expand=nn.Conv2d(D, out_channels, kernel_size=1, stride=1, padding=0, bias=False)
+        self.bn_expand=nn.BatchNorm2d(out_channels)
+
+        self.shortcut=nn.Sequential()
+        if in_channels!=out_channels:
+            self.shortcut.add_module('shortcut_conv', nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, padding=0, bias=False))
+            self.shortcut.add_module('shortcut_bn', nn.BatchNorm2d(out_channels))
+    
+    def forward(self, x):
+        bottleneck=self.conv_reduce.forward(x)
+        bottleneck=F.relu(self.bn_reduce.forward(bottleneck), inplace=True)
+        bottleneck=self.conv_conv.forward(bottleneck)
+        bottleneck=F.relu(self.bn.forward(bottleneck), inplace=True)
+        bottleneck=self.conv_expand.forward(bottleneck)
+        bottleneck=self.bn_expand.forward(bottleneck)
+        residual=self.shortcut.forward(x)
+        return F.relu(residual+bottleneck, inplace=True)
+
+
+class CifarResNeXt(nn.Module):
+    def __init__(self, cardinality, depth, nlabels, base_width, widen_factor=4):
+        super(CifarResNeXt, self).__init__()
+        self.cardinality=cardinality
+        self.depth=depth
+        self.block_depth=(self.depth-2)//9
+        self.base_width=base_width
+        self.widen_factor=widen_factor
+        self.nlabels=nlabels
+        self.output_size=64
+        self.stages=[64, 64*self.widen_factor, 128*self.widen_factor, 256*self.widen_factor]
+
+        self.conv_1_3x3=nn.Conv2d(3, 64, 3, 1, 1, bias=False)
+        self.bn_1=nn.BatchNorm2d(64)
+        self.stage_1=self.block('stage_1', self.stages[0], self.stages[1], 1)
+        self.stage_2=self.block('stage_2', self.stages[1], self.stages[2], 2)
+        self.stage_3=self.block('stage_3', self.stages[2], self.stages[3], 2)
+        self.classifier=nn.Linear(self.stages[3], nlabels)
+
+        for key in self.state_dict():
+            if key.split('.')[-1]=='weight':
+                if 'conv' in key:
+                    init.kaiming_normal(self.state_dict()[key], mode='fan_out')
+                if 'bn' in key:
+                    self.state_dict()[key][...]=1
+            elif key.split('.')[-1]=='bias':
+                self.state_dict()[key][...]=0
+
+    def block(self, name, in_channels, out_channels, pool_stride=2):
+        block=nn.Sequential()
+        for bottleneck in range(self.block_depth):
+            name_='%s_bottlenect_%d' % (name, bottleneck)
+            if bottleneck==0:
+                block.add_module(name_, ResNeXtBottleneck(in_channels, out_channels, pool_stride, self.cardinality, self.base_width, self.widen_factor))
+            else:
+                block.add_module(name_, ResNeXtBottleneck(out_channels, out_channels, 1, self.cardinality, self.base_width, self.widen_factor))
+        return block
+
+    def forward(self, x):
+        x=self.conv_1_3x3.forward(x)
+        x=F.relu(self.bn_1.forward(x), inplace=True)
+        x=self.stage_1.forward(x)
+        x=self.stage_2.forward(x)
+        x=self.stage_3.forward(x)
+        x=F.avg_pool2d(x, 8, 1)
+        x=x.view(-1, self.stages[3])
+        return self.classifier(x)
